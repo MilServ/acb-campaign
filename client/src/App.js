755abf0e81e27234c81472e16092642dd6c39ad8
Juan Rivera <jrivera@primalplatoon.com>
2020-09-30 12:16:54 -0400

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "../src/components/Header";
 import "./App.css";
 import LandingPage from "./pages/LandingPage";
+import { initGA, logPageView } from "./utils/analytics";
 
 function App() {
+  useEffect(() => {
+    if (!window.GA_INITIALIZED) {
+      initGA();
+      window.GA_INITIALIZED = true;
+    }
+    logPageView();
+  });
   return (
     <div className="container">
       <Header />
